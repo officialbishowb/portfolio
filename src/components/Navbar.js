14b7edd66efab459a8655bcf53a6c008afffcd64
Bishow Bhattarai <officialbishowb@gmail.com>
2024-12-23 15:06:58 +0100

    feat: enhance Hero and Navbar components; improve text size, add tooltip, and adjust styles for better responsiveness

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,18 +3,18 @@
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import styles from '@/app/styles/components/Navbar.module.scss';
-import { FaRegUserCircle ,FaExternalLinkSquareAlt  , FaEnvelope, FaBlog, FaProjectDiagram } from 'react-icons/fa';
+import { FaRegUserCircle, FaExternalLinkSquareAlt, FaEnvelope, FaBlog, FaProjectDiagram } from 'react-icons/fa';
 import { IoHomeOutline } from "react-icons/io5";
 const Navbar = () => {
     const pathname = usePathname();
 
     return (
-        <nav className="fixed bottom-20 left-1/2 transform -translate-x-1/2 sm:top-10 sm:left-1/2 sm:transform sm:-translate-x-1/2 sm:bottom-auto border border-gray-500 rounded-lg z-50 bg-background flex justify-center items-center p-2 shadow-lg">
-            <ul className="flex flex-row justify-center items-center space-x-4">
+        <nav className="fixed bottom-20 left-1/2 transform -translate-x-1/2 sm:top-10 sm:left-1/2 sm:transform sm:-translate-x-1/2 sm:bottom-auto border border-gray-500 rounded-lg z-50 sm:bg-opacity-50 backdrop-filter-none bg-background sm:bg-transparent sm:backdrop-blur-2xl flex justify-center items-center p-2 shadow-lg">
+            <ul className="flex flex-row justify-center items-center space-x-1 sm:space-x-4">
                 <li>
                     <Link href="/">
                         <div className={`${styles.navItem} ${pathname === '/' ? styles.navItemSelected : ''} text-white flex flex-row items-center space-x-2`}>
-                            <IoHomeOutline  />
+                            <IoHomeOutline />
                             <p className="hidden sm:block">Home</p>
                         </div>
                     </Link>
@@ -35,7 +35,6 @@ const Navbar = () => {
                         </div>
                     </Link>
                 </li>
-                
                 <li>
                     <Link href="/#contact">
                         <div className={`${styles.navItem} ${pathname === '/#contact' ? styles.navItemSelected : ''} text-white flex flex-row items-center space-x-2`}>
@@ -48,7 +47,7 @@ const Navbar = () => {
                     <Link href="https://blog.officialbishowb.com" target="_blank" rel="noopener noreferrer">
                         <div className={`${styles.navItem} text-white flex flex-row items-center space-x-2`}>
                             <FaBlog />
-                            <p className="hidden sm:block relative">Blog
+                            <p className="relative"><span className="hidden sm:block">Blog</span>
                                 <FaExternalLinkSquareAlt className="absolute top-[-8px] right-[-15px] text-[.7rem] text-white" />
                             </p>
                         </div>
