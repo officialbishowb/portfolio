ec1c37442179844b0abb0c08c741beec69f8204a
Bishow Bhattarai <officialbishowb@gmail.com>
2024-12-26 10:21:11 +0100

    fix: remove unused image domain from Next.js configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['placehold.co','avatars.githubusercontent.com'], 
+        domains: ['avatars.githubusercontent.com'], 
         dangerouslyAllowSVG: true,
     },
 };
